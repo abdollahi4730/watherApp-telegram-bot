c5befa81d22b3235a713719361b25d8347e86d42
Mohammad <mmmmmmmm.mohammad@gmail.com>
2024-03-10 11:45:14 +0330

    test Del

--- key.py
+++
@@ -1,8 +0,0 @@
-class CrKey:
-    def __init__(self , username : str , password : str) -> None:
-        self.username = username
-        self.password = password
-
-username = "40224423"
-password = "4730"
-key = CrKey(username= username , password= password)
